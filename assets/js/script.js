97d5603831afe79f4222ba5e37ae5a841b84e6ef
Kaia Scheirman <kfscheirman18@gmail.com>
2022-02-17 20:57:15 -0800

    add ability to drag/drop tasks into trash

--- assets/js/script.js
+++ assets/js/script.js
@@ -190,6 +190,21 @@ $(".card .list-group").sortable({
   }
 });
 
+// add ability to drop tasks into trash
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    
+  },
+  out: function(event, ui) {
+   
+  }
+});
+
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
